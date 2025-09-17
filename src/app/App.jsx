879489fe81d72ef7f5abb8604d35e81f60366576
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-09-17 19:45:46 +0300

    Context

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,18 +4,39 @@ import './ui/App.css'
 import Layout from '../widgets/layout/Layout';
 import Category from '../pages/category/Category';
 import Home from '../pages/home/Home';
+import AppContext from '../features/context/AppContext';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="category" element={<Category />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>;
+  const backUrl = "https://localhost:7278";
+
+  const request = (url, conf) => new Promise((resolve, reject) => {
+    if(url.startsWith('/')) {
+      url = backUrl + url;
+    }
+    fetch(url)
+        .then(r => r.json())
+        .then(j => {
+            if(j.status.isOk) {
+                resolve(j.data);
+            }
+            else {
+                reject(j);
+            }
+        });
+  });
+
+  return <AppContext.Provider value={ { request, backUrl } }>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="category" element={<Category />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppContext.Provider>;
 }
 
 export default App
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import AppContext from "../../features/context/AppContext";
 
 export default  function Home() {
+    const {request, backUrl} = useContext(AppContext);
     const [groups, setGroups] = useState([]);
 
     useEffect(() => {
-        fetch("https://localhost:7278/api/group")
-        .then(r => r.json())
-        .then(setGroups);
+        request("/api/group")
+        .then(setGroups)
+        .catch(console.error);
     }, []);
 
     return <>        
@@ -18,7 +20,7 @@ export default  function Home() {
             {groups.map(group => <div key={group.id} className="col">
                 <Link className="nav-link h-100" to="Category" asp-route-id={group.slug}>
                     <div className="card h-100">
-                        <img src={"https://localhost:7278/Storage/Item/" + group.imageUrl} className="card-img-top" alt="..." />
+                        <img src={backUrl + "/Storage/Item/" + group.imageUrl} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{group.name}</h5>
                             <p className="card-text">{group.description}</p>
